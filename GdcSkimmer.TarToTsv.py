399f1dfa03aa54e13f263344f7db02a5a391b869
arose <andrew.william.rose@cern.ch>
2022-08-12 19:10:09 +0100

    Move the old skimmer back to main code and update

--- GdcSkimmer.TarToTsv.py
+++ GdcSkimmer.TarToTsv.py
@@ -18,23 +18,30 @@ Genes = [ "ATRX" , "GAPDH" , "TUBA1A" , "ACTB" , "ALB" , "ALOX12" , "ANGPTL7" , 
           "PREX1" , "PRG3" , "PRNP" , "PTGS1" , "PTGS2" , "PXDN" , "PXDNL" , "RNF7" , "SCARA3" , "SELENOS" , "SELENOP" , "SFTPD" , 
           "SGK2" , "SIRT2" , "SOD1" , "SOD2" , "SOD3" , "SRXN1" , "STK25" , "TPO" , "TTN" , "TXNDC2" , "TXNRD1" , "TXNRD2" ]
 
+Mutations = [ "ATRX" , "IDH1" , "SETD2" ]
+
 with open( args.dest , "w" ) as dest:                                    # Open the destination tsv and call the file-handle 'dest'
   lCases = LoadFormattedTarball( args.src , Genes )                      # Load the GDC data from the formatted tarball as a dictionary of CaseId strings and Case objects
                                                                          # keeping only the specified Genes from the star-count file
+  dest.write( "CaseId" ) # Write the header-line to the destination tsv
+  for i in Mutations : dest.write( "\tMutations-" + i )
+  for i in Genes :     dest.write( "\tStarCount-" + i )
+  dest.write( "\n" )
+  
+  
+  for lCaseId , lCase in sorted( lCases.items() ):                       # Sort the cases by case-id, then iterate over them storing the CaseId and Case in separate variables    
+    if lCase.StarCounts is None: continue                                # If there is no star-count info...
 
-  dest.write( "CaseId\tATRX-mutation\t" + "\t".join( Genes ) + "\n" )    # Write the header-line to the destination tsv 
-  for lCaseId , lCase in sorted( lCases.items() ):                       # Sort the cases by case-id, then iterate over them storing the CaseId and Case in separate variables
+    for lStarCount in lCase.StarCounts:                                # Else, iterate over each star-count file
+      dest.write( lCaseId )
 
-    lMutations = lCase.getMutations( "ATRX" )                            # Get any mutations of ATRX
-    if lMutations is None : lMutations = "No WXS Data"                   # If there is no info, say so
-    else: lMutations = " , ".join( [ str(i) for i in lMutations ] )      # Else pack all mutations into a single comma-delimited string
+      for i in Mutations:
+        lMutations = lCase.getMutations( i )
+        if lMutations is None: dest.write( "\tNo WXS Data" )
+        else:                  dest.write( "\t" + ( " , ".join( list( set( [ str(j) for j in lMutations ] ) ) ) ) )         
     
-    if lCase.StarCounts is None:                                         # If there is no star-count info...
-      dest.write( f"{lCaseId}\t{lMutations}\tNo star-count\n" )          #   Write the information that is available to the destination file using python 3's f-strings
-    else:
-      for lStarCount in lCase.StarCounts:                                # Else, iterate over each star-count file
-        lStarCount = [ str( lStarCount[i] ) for i in Genes ]             #   Convert the dictionary for that star-count file into a list of strings in the same order as the genes list
-        lStarCount = "\t".join( lStarCount )                             #   Convert the list to a single tab-delimeted string
-        dest.write( f"{lCaseId}\t{lMutations}\t{lStarCount}\n" )         #   Write the information to the destination file using python 3's f-strings
+      for i in Genes: dest.write( "\t" + str( lStarCount[i] ) )
+    
+      dest.write( "\n" )  
 # ==============================================================================================================================================
 
