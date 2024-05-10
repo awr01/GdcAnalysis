08d432b2446029058a6a57ff4989ea5318274eea
arose <andrew.william.rose@cern.ch>
2024-05-10 11:17:21 +0100

    Tidy up

--- GdcCombinedVolcano.py
+++ GdcCombinedVolcano.py
@@ -23,18 +23,6 @@ def Classifier( aCase ):
 # ======================================================================================================
 
 
-# # ======================================================================================================
-# def SortByDisease( aCase ):    
-#   index = 1                                                                      # Indices chosen for consistency with old code
-#   if MutationOfInterest in aCase.Mutations: 
-#     if aCase.Mutations[ MutationOfInterest ].Classification == SilentOrSplice : return
-#     index = 0
-
-#   lDiseaseType = str( aCase.DiseaseType )
-#   if not lDiseaseType in lCases2: lCases2[ lDiseaseType ] = [ [] , [] ]
-#   lCases2[ lDiseaseType ][ index ].append( aCase.CaseId )
-# # ======================================================================================================
-
 # ======================================================================================================
 def Flatten( Data , index ):
   lRet = []      
@@ -42,44 +30,7 @@ def Flatten( Data , index ):
     for i in j.StarCounts: 
       lRet.append( i.TpmUnstranded[ index ] )
   return lRet
-# ======================================================================================================
 
-# # ======================================================================================================
-# def DataToScatter( lArgs ):
-#   lDiseaseType , lCases = lArgs
-#   index = multiprocessing.current_process()._identity[0]
-
-#   if len( lCases[0] ) == 0 or len( lCases[1] ) == 0 : return
-
-#   lMut , lWt = LoadCases( args.src , lCases[0] ) , LoadCases( args.src , lCases[1] )
-  
-#   x0 , y0 , x1 , y1 , x2 , y2 , x3 , y3 , x4 , y4 = [] , [] , [] , [] , [] , [] , [] , [] , [] , []
-#   for GeneName , Gene in tqdm.tqdm( sorted( StarCounts.GeneCatalogue.items() ) , leave=False , ncols=Ncol , desc=lDiseaseType, position=index ):
-#     if Gene.type != "protein_coding" : continue
-#     lRet = GdcStatistics( Flatten( lMut , Gene.index ) , Flatten( lWt , Gene.index ) )
-#     if lRet is None : continue
-#     if np.isnan( lRet.neg_log_pvalue ) : continue
-#     if ( GeneName == "DRG2" ):  
-#       x0.append( lRet.log_mean_ratio_with_error[0] )
-#       y0.append( lRet.neg_log_pvalue )
-#     elif ( GeneName == "TRIM24" ):  
-#       x3.append( lRet.log_mean_ratio_with_error[0] )
-#       y3.append( lRet.neg_log_pvalue )
-#     elif ( GeneName == "SLFN11" ):  
-#       x4.append( lRet.log_mean_ratio_with_error[0] )
-#       y4.append( lRet.neg_log_pvalue )      
-#     elif ( lRet.neg_log_pvalue < 2.3 ) or ( np.fabs( lRet.log_mean_ratio_with_error[0] ) < 1 ):  
-#       x1.append( lRet.log_mean_ratio_with_error[0] )
-#       y1.append( lRet.neg_log_pvalue )
-#     else:
-#       x2.append( lRet.log_mean_ratio_with_error[0] )
-#       y2.append( lRet.neg_log_pvalue )   
-
-#   return lDiseaseType , ( ( x0 , y0 ), ( x1 , y1 ), ( x2 , y2 ), ( x3 , y3 ), ( x4 , y4 ) )  
-# # ======================================================================================================
-
-
-# ======================================================================================================
 def ForEachClass( Class , Cases , index ):
   lMut , lWt = [] , []
 
@@ -108,16 +59,6 @@ def ForEachClass( Class , Cases , index ):
 
   return ( ( x0 , y0 ), ( x1 , y1 ), ( x2 , y2 ) )  
 # ======================================================================================================
-
-
-# # ======================================================================================================
-# def Finally():
-#   # Fill the plots 
-#   with multiprocessing.Pool() as pool:
-#     for res in tqdm.tqdm( pool.imap( DataToScatter , sorted( lCases2.items() ) ) , ncols=Ncol , desc="Analysing" ):
-#       if not res is None: Data[ res[0] ] = res[1]
-# # ======================================================================================================
-
 
 
 # ======================================================================================================
@@ -157,17 +98,6 @@ def DrawVolcanos( Data ):
 # ======================================================================================================
 if len( args.mutations ) != 1 : raise Exception( "Exactly 1 mutation must be specified on the commandline" )
 MutationOfInterest, Dict = args.mutations[0], {}
-
-# lFile = f".cache/GdcVolcanoRaw.pkl.bz"
-
-# if os.path.isfile( lFile ):
-#   print( "> Using cached stats" , flush=True )
-#   with bz2.BZ2File( lFile , 'r' ) as src: Data = _pickle.load( src )  
-# else:
-#   print( "> Extracting stats" , flush=True )
-# LoadAndForEach( args.src , SortByDisease , After = Finally ) # Load src and analyze on-the-fly, making use of the optional After function
-# with bz2.BZ2File( lFile , 'w' ) as dest: _pickle.dump( Data , dest )
-
 LoadAndClassify( args.src , Classifier , ForEachClass , DrawVolcanos , maxthreads=3 )
 # ======================================================================================================
   
