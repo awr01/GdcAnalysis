04d45d87df9688c095ee7d01b22a837004b1d067
arose <andrew.william.rose@cern.ch>
2024-05-12 07:45:31 +0100

    Move Analysis to new framework

--- GdcAnalysis.py
+++ GdcAnalysis.py
@@ -1,18 +1,102 @@
 from GdcLib import *
 from GdcUtils import *
 from openpyxl import Workbook
+import numpy as np
+import os , hashlib , bz2 , _pickle
 
-# ======================================================================================================
-def SortByDisease( aCase ):    
-  index = 1                                                                      # Indices chosen for consistency with old code
-  if MutationOfInterest in aCase.Mutations: 
-    if aCase.Mutations[ MutationOfInterest ].Classification == SilentOrSplice : return
-    index = 0
+# # ======================================================================================================
+# def SortByDisease( aCase ):    
+#   index = 1                                                                      # Indices chosen for consistency with old code
+#   if MutationOfInterest in aCase.Mutations: 
+#     if aCase.Mutations[ MutationOfInterest ].Classification == SilentOrSplice : return
+#     index = 0
 
-  lDiseaseType = str( aCase.DiseaseType )
-  if not lDiseaseType in lCases2: lCases2[ lDiseaseType ] = [ [] , [] ]
-  lCases2[ lDiseaseType ][ index ].append( aCase )
-# ======================================================================================================
+#   lDiseaseType = str( aCase.DiseaseType )
+#   if not lDiseaseType in lCases2: lCases2[ lDiseaseType ] = [ [] , [] ]
+#   lCases2[ lDiseaseType ][ index ].append( aCase )
+# # ======================================================================================================
+
+# # ======================================================================================================
+# def Flatten( Data , index ):
+#   lRet = []      
+#   for j in Data:
+#     for i in j.StarCounts: 
+#       lRet.append( i.TpmUnstranded[ index ] )
+#   return lRet
+# # ======================================================================================================
+
+# # ======================================================================================================
+# def Finally():
+#   for lDiseaseType , lCases in tqdm.tqdm( sorted( lCases2.items() ) , ncols=Ncol , desc="Analysing" ):
+#     lMut , lWt = lCases[0] , lCases[1]
+#     if len( lMut ) == 0 or len( lWt ) == 0 : continue
+
+#     HeaderFn( lDiseaseType )
+    
+#     for GeneName , Gene in tqdm.tqdm( sorted( StarCounts.GeneCatalogue.items() ) , leave=False , ncols=Ncol , desc=lDiseaseType ):
+#       if Gene.type != "protein_coding" : continue
+#       lRet = GdcStatistics( Flatten( lMut , Gene.index ) , Flatten( lWt , Gene.index ) )
+#       if lRet is None : continue
+#       OutputFn( lDiseaseType , GeneName , lRet )   
+# # ======================================================================================================
+
+# # ======================================================================================================
+# def HeaderTsv( DiseaseType ):
+#   dest.write( f"Disease-type\tGene\t" )
+#   dest.write( f"Mut-count\tMut-mean\tMut-mean-error\tMut-std.dev\t" )
+#   dest.write( f"WT-count\tWT-mean\tWT-mean-error\tWT-std.dev\t" )
+#   dest.write( f"Mut mean/WT mean\terror(Mut mean/WT mean)\t" )
+#   dest.write( f"log_1.5(ratio)\terror(log_1.5(ratio))\t" )
+#   dest.write( f"t-score\tp-value\tneg log_10(p-value)\n" ) # Write headers  
+
+# def OutputTsv( DiseaseType , GeneName , aStats ):
+#   dest.write( f"{DiseaseType}\t{GeneName}\t" )
+#   dest.write( f"{aStats.mut.count}\t{aStats.mut.mean}\t{aStats.mut.mean_error}\t{aStats.mut.sd}\t" )
+#   dest.write( f"{aStats.wt.count }\t{aStats.wt.mean}\t{aStats.wt.mean_error}\t{aStats.wt.sd}\t" )
+#   dest.write( f"{aStats.mean_ratio_with_error[0]}\t{aStats.mean_ratio_with_error[1]}\t")
+#   dest.write( f"{aStats.log_mean_ratio_with_error[0]}\t{aStats.log_mean_ratio_with_error[1]}\t" )
+#   dest.write( f"{aStats.tscore}\t{aStats.pvalue}\t{aStats.neg_log_pvalue}\n" )
+# # ======================================================================================================
+
+# # ======================================================================================================
+# def HeaderXlsx( DiseaseType ):
+#   global ws
+#   ws = wb.create_sheet( DiseaseType )
+#   ws.append( [ "Gene" , 
+#                 "Mut-count" , "Mut-mean" , "Mut-mean-error" , "Mut-std.dev" , 
+#                 "WT-count" , "WT-mean" , "WT-mean-error" , "WT-std.dev" , 
+#                 "Mut mean/WT mean" , "error(Mut mean/WT mean)" , 
+#                 "log_1.5(ratio)" , "error(log_1.5(ratio))" , 
+#                 "t-score" , "p-value" , "neg log_10(p-value)" ] ) # Write headers
+
+# def OutputXlsx( DiseaseType , GeneName , aStats ):
+#   if aStats is None: return
+#   ws.append( [ GeneName , 
+#                 aStats.mut.count , aStats.mut.mean , aStats.mut.mean_error , aStats.mut.sd , 
+#                 aStats.wt.count  , aStats.wt.mean  , aStats.wt.mean_error  , aStats.wt.sd , 
+#                 aStats.mean_ratio_with_error[0] , aStats.mean_ratio_with_error[1] , 
+#                 aStats.log_mean_ratio_with_error[0] , aStats.log_mean_ratio_with_error[1] , 
+#                 aStats.tscore , aStats.pvalue , aStats.neg_log_pvalue ] )
+# # ======================================================================================================
+
+# # ======================================================================================================
+# if len( args.mutations ) != 1 : raise Exception( "Exactly 1 mutation must be specified on the commandline" )
+# MutationOfInterest , lCases2 = args.mutations[0] , {}         
+
+# if args.dest.endswith( ".tsv" ):
+#   with open( args.dest , "w" ) as dest:
+#     HeaderFn , OutputFn = HeaderTsv , OutputTsv
+#     LoadAndForEach( args.src , SortByDisease , After = Finally ) # Load src and analyze on-the-fly, making use of the optional After function
+# else:
+#   with open( args.dest , "w" ) as dest: pass
+#   HeaderFn , OutputFn = HeaderXlsx , OutputXlsx
+#   wb , ws = Workbook() , None
+#   wb.remove_sheet( wb.active ) # Delete default sheet
+#   LoadAndForEach( args.src , SortByDisease , After = Finally ) # Load src and analyze on-the-fly, making use of the optional After function
+#   wb.save( args.dest )    
+  
+# # ======================================================================================================
+
 
 # ======================================================================================================
 def Flatten( Data , index ):
@@ -21,77 +105,73 @@ def Flatten( Data , index ):
     for i in j.StarCounts: 
       lRet.append( i.TpmUnstranded[ index ] )
   return lRet
+
+def ForEachClass( Class , Cases , index ):
+  lMut , lWt = [] , []
+
+  for Case in Cases:
+    if MutationOfInterest in Case.Mutations: 
+      if Case.Mutations[ MutationOfInterest ].Classification != SilentOrSplice : lMut.append( Case )   
+    else: lWt.append( Case )  
+
+  if len( lMut ) == 0 or len( lWt ) == 0 : return
+  
+  Results = {}
+  for GeneName , Gene in tqdm.tqdm( sorted( StarCounts.GeneCatalogue.items() ) , leave=False , ncols=Ncol , desc=f"{Class}: Analysing", position=index ):
+    if Gene.type != "protein_coding" : continue
+    lRet = GdcStatistics( Flatten( lMut , Gene.index ) , Flatten( lWt , Gene.index ) )
+    if lRet is None: continue
+    if np.isnan( lRet.neg_log_pvalue ) : continue
+
+    Results[ GeneName ] = lRet
+
+  return Results
 # ======================================================================================================
 
-# ======================================================================================================
-def Finally():
-  for lDiseaseType , lCases in tqdm.tqdm( sorted( lCases2.items() ) , ncols=Ncol , desc="Analysing" ):
-    lMut , lWt = lCases[0] , lCases[1]
-    if len( lMut ) == 0 or len( lWt ) == 0 : continue
-
-    HeaderFn( lDiseaseType )
-    
-    for GeneName , Gene in tqdm.tqdm( sorted( StarCounts.GeneCatalogue.items() ) , leave=False , ncols=Ncol , desc=lDiseaseType ):
-      if Gene.type != "protein_coding" : continue
-      lRet = GdcStatistics( Flatten( lMut , Gene.index ) , Flatten( lWt , Gene.index ) )
-      if lRet is None : continue
-      OutputFn( lDiseaseType , GeneName , lRet )   
-# ======================================================================================================
 
 # ======================================================================================================
-def HeaderTsv( DiseaseType ):
-  dest.write( f"Disease-type\tGene\t" )
-  dest.write( f"Mut-count\tMut-mean\tMut-mean-error\tMut-std.dev\t" )
-  dest.write( f"WT-count\tWT-mean\tWT-mean-error\tWT-std.dev\t" )
-  dest.write( f"Mut mean/WT mean\terror(Mut mean/WT mean)\t" )
-  dest.write( f"log_1.5(ratio)\terror(log_1.5(ratio))\t" )
-  dest.write( f"t-score\tp-value\tneg log_10(p-value)\n" ) # Write headers  
+def ExportXlsx( Data ): 
 
-def OutputTsv( DiseaseType , GeneName , aStats ):
-  dest.write( f"{DiseaseType}\t{GeneName}\t" )
-  dest.write( f"{aStats.mut.count}\t{aStats.mut.mean}\t{aStats.mut.mean_error}\t{aStats.mut.sd}\t" )
-  dest.write( f"{aStats.wt.count }\t{aStats.wt.mean}\t{aStats.wt.mean_error}\t{aStats.wt.sd}\t" )
-  dest.write( f"{aStats.mean_ratio_with_error[0]}\t{aStats.mean_ratio_with_error[1]}\t")
-  dest.write( f"{aStats.log_mean_ratio_with_error[0]}\t{aStats.log_mean_ratio_with_error[1]}\t" )
-  dest.write( f"{aStats.tscore}\t{aStats.pvalue}\t{aStats.neg_log_pvalue}\n" )
+  # ------------------------------------------------------
+  with open( args.src , "rb") as f:
+    file_hash = hashlib.md5()
+    while chunk := f.read(8192): file_hash.update(chunk)
+
+  if not os.path.isdir( ".cache" ): os.mkdir( ".cache" )
+  lFile = f".cache/Comutation.{MutationOfInterest}.{file_hash.hexdigest()}.pkl.bz"
+  with bz2.BZ2File( lFile , 'w' ) as dest: _pickle.dump( Data , dest )
+  # ------------------------------------------------------
+
+  wb , ws = Workbook() , None
+  wb.remove_sheet( wb.active ) # Delete default sheet
+
+  for lDiseaseType , lData in Data.items():
+
+    if lData is None: continue
+
+    ws = wb.create_sheet( lDiseaseType )
+    ws.append( [ "Gene" , 
+                  "Mut-count" , "Mut-mean" , "Mut-mean-error" , "Mut-std.dev" , 
+                  "WT-count"  , "WT-mean"  , "WT-mean-error"  , "WT-std.dev" , 
+                  "Mut mean/WT mean" , "error(Mut mean/WT mean)" , 
+                  "log_1.5(ratio)" , "error(log_1.5(ratio))" , 
+                  "t-score" , "p-value" , "neg log_10(p-value)" ] ) # Write headers
+
+    for GeneName, aStats in sorted( lData.items() , key = lambda x : x[1].pvalue ):
+      ws.append( [ GeneName , 
+                    aStats.mut.count , aStats.mut.mean , aStats.mut.mean_error , aStats.mut.sd , 
+                    aStats.wt.count  , aStats.wt.mean  , aStats.wt.mean_error  , aStats.wt.sd , 
+                    aStats.mean_ratio_with_error[0] , aStats.mean_ratio_with_error[1] , 
+                    aStats.log_mean_ratio_with_error[0] , aStats.log_mean_ratio_with_error[1] , 
+                    aStats.tscore , aStats.pvalue , aStats.neg_log_pvalue ] )
+
+  wb.save( args.dest ) 
+ 
 # ======================================================================================================
 
-# ======================================================================================================
-def HeaderXlsx( DiseaseType ):
-  global ws
-  ws = wb.create_sheet( DiseaseType )
-  ws.append( [ "Gene" , 
-                "Mut-count" , "Mut-mean" , "Mut-mean-error" , "Mut-std.dev" , 
-                "WT-count" , "WT-mean" , "WT-mean-error" , "WT-std.dev" , 
-                "Mut mean/WT mean" , "error(Mut mean/WT mean)" , 
-                "log_1.5(ratio)" , "error(log_1.5(ratio))" , 
-                "t-score" , "p-value" , "neg log_10(p-value)" ] ) # Write headers
-
-def OutputXlsx( DiseaseType , GeneName , aStats ):
-  if aStats is None: return
-  ws.append( [ GeneName , 
-                aStats.mut.count , aStats.mut.mean , aStats.mut.mean_error , aStats.mut.sd , 
-                aStats.wt.count  , aStats.wt.mean  , aStats.wt.mean_error  , aStats.wt.sd , 
-                aStats.mean_ratio_with_error[0] , aStats.mean_ratio_with_error[1] , 
-                aStats.log_mean_ratio_with_error[0] , aStats.log_mean_ratio_with_error[1] , 
-                aStats.tscore , aStats.pvalue , aStats.neg_log_pvalue ] )
-# ======================================================================================================
 
 # ======================================================================================================
 if len( args.mutations ) != 1 : raise Exception( "Exactly 1 mutation must be specified on the commandline" )
-MutationOfInterest , lCases2 = args.mutations[0] , {}         
-
-if args.dest.endswith( ".tsv" ):
-  with open( args.dest , "w" ) as dest:
-    HeaderFn , OutputFn = HeaderTsv , OutputTsv
-    LoadAndForEach( args.src , SortByDisease , After = Finally ) # Load src and analyze on-the-fly, making use of the optional After function
-else:
-  with open( args.dest , "w" ) as dest: pass
-  HeaderFn , OutputFn = HeaderXlsx , OutputXlsx
-  wb , ws = Workbook() , None
-  wb.remove_sheet( wb.active ) # Delete default sheet
-  LoadAndForEach( args.src , SortByDisease , After = Finally ) # Load src and analyze on-the-fly, making use of the optional After function
-  wb.save( args.dest )    
-  
+MutationOfInterest = args.mutations[0]       
+LoadAndClassify( args.src , lambda aCase: str( aCase.DiseaseType ) , ForEachClass , ExportXlsx )
 # ======================================================================================================
-  
--- GdcUtils.py
+++ GdcUtils.py
@@ -1,5 +1,5 @@
 import argparse
-from GdcUtils import *
+# from GdcUtils import *
 
 
 parser = argparse.ArgumentParser()
@@ -9,21 +9,21 @@ parser.add_argument( '--dest' , required=True , help='The destination file')
 parser.add_argument( '--genes' , nargs='+' , default=[] , help='Genes of interest')
 parser.add_argument( '--mutations' , nargs='+' , default=[] , help='Mutations of interest')
 
-parser.add_argument( '--gene-file' , help='Genes of interest')
-parser.add_argument( '--mutation-file' , help='Mutations of interest')
+# parser.add_argument( '--gene-file' , help='Genes of interest')
+# parser.add_argument( '--mutation-file' , help='Mutations of interest')
 
 args = parser.parse_args()
 
 if not args.src .endswith( ".tar"  ): raise Exception( "Source file must have '.tar' file-extension" )
-if not ( args.dest.endswith( ".tsv" ) or args.dest.endswith( ".xlsx" ) ): raise Exception( "Destination file must have '.tsv' or '.xlsx' file-extension" )
+if not args.dest.endswith( ".xlsx" ): raise Exception( "Destination file must have '.xlsx' file-extension" )
 
-if not args.gene_file is None:
-  with open( args.gene_file , "r" ) as src:
-    for line in src: args.genes.append( line.strip() )
+# if not args.gene_file is None:
+#   with open( args.gene_file , "r" ) as src:
+#     for line in src: args.genes.append( line.strip() )
 
-if not args.mutation_file is None:
-  with open( args.mutation_file , "r" ) as src:
-    for line in src: args.mutations.append( line.strip() )
+# if not args.mutation_file is None:
+#   with open( args.mutation_file , "r" ) as src:
+#     for line in src: args.mutations.append( line.strip() )
 
 
 
